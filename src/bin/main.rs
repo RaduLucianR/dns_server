cb969eeab514f21633353ec3b3fe2d354adcc17b
RaduLucianR <radlucradulescu@gmail.com>
2026-01-07 21:51:50 +0100

    pkt structs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,13 +1,23 @@
 use std::fs::File;
 use std::io::Read;
 
-fn main() -> std::io::Result<()> {
-    let mut file = File::open("response_packet.txt")?;
-    let mut words: [u8; 512] = [0; 512];
-    file.read(&mut words)?;
+pub struct DNSPacket {
+    pub buffer: [u8; 512],
+}
 
-    for word in words {
-        println!("{}", word);
+impl DNSPacket {
+    pub fn new() -> DNSPacket {
+        DNSPacket { buffer: [0; 512] }
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let mut file = File::open("query_packet.txt")?;
+    let mut packet = DNSPacket::new();
+    file.read(&mut packet.buffer)?;
+
+    for i in 0..8 {
+        println!("{}", packet.buffer[i]);
     }
 
     Ok(())
